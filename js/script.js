0f6ed34649e7164c39790b2a349bf4e2cc6b049a
Derek Marler <derek@dereks-mbp.attlocal.net>
2018-02-14 17:05:37 -0800

    Mobile versions done, some various fixes. Should be good to go.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 /*
+ * Mobile Check
+ */
+var isMobile;
+function checkMobile() {
+    isMobile = (window.innerWidth >= 1080) ? false : true;
+}
+
+// Check for mobile on browser resize
+$(window).resize(function () {
+    checkMobile();
+});
+
+/*
  * Gets position/size of list item and animates onto modal
  */
 function animateThumb(el) {
@@ -9,20 +22,19 @@ function animateThumb(el) {
     // Set width to animate from
     $('.modal-image').css('width', $(el).width());
 
-    // Find X and Y it needs to move too.
-    var newWidth = window.innerWidth*0.50;
-    var newX = (window.innerWidth*0.02)-thumb.left; // 2% of window width minus current list item position, negative value
-    var newY = ((window.innerHeight*0.5)-thumb.top)-((newWidth*0.75)/2)-5; // get distance from top, minus by half of new image height (55vw*75%/2) minus border
-
     // Set modal image to list item dimensions
     $('.modal-image')
         .css({
             'background-image':$(el).css('background-image'),
             left   : thumb.left,
             top    : thumb.top,
-            transform : 'translateX('+newX+'px) translateY('+newY+'px)',
             width  : thumb.width
         });
+
+    // Get width, X, and Y values to move to
+    var newWidth = window.innerWidth*0.5; // 50% of viewport
+    var newX = (window.innerWidth*0.02)-thumb.left; // 2% of window width minus current list item position, negative value
+    var newY = ((window.innerHeight*0.5)-thumb.top)-((newWidth*0.75)/2)-6; // get distance from top, minus by half of new image height (55vw*75%/2) minus border
     
     // Prevent flicker when animating and hiding
     setTimeout(function() { 
@@ -31,7 +43,8 @@ function animateThumb(el) {
         $('.modal-image')
             .addClass('animate-in')
             .css({
-                width : newWidth
+                transform   : 'translateX('+newX+'px) translateY('+newY+'px)',
+                width       : newWidth
             });
         
         // Hide list item in time with animation
@@ -79,29 +92,54 @@ function populateModal(el) {
  */
 $('.portfolio li').click(function(e) {
     e.preventDefault();
-    animateThumb(this);
+
+    // Prevent parent scrolling
+    $('body').addClass('modal-open');
+
+    if(!isMobile) {
+        // Animate thumb if desktop
+        animateThumb(this);
+    } else {
+        // Mobile, just swap image
+        $('.modal-image-mobile').css('background-image',$(this).css('background-image'));
+    }
+
+    // Populate data
     populateModal(this);
+
+    // Show modal
     $('.modal').addClass('show');
 });
 
 /*
  * Close Modal
  */
-$('.modal-image, .modal-close').click(function(e) {
+$('.modal-image, .modal-image-mobile, .modal-close').click(function(e) {
     e.preventDefault();
+    
+    // Hide modal
     $('.modal').removeClass('show');
-    setTimeout(function() {
-        // Move it back to the list and disappear
-        $('.modal-image').removeClass('animate-in').css({
-            transform : 'translateX(0) translateY(0)',
-            width : 600
-        });
-        // Wait for transition to finish...
+    
+    if(!isMobile) {
         setTimeout(function() {
-            // Show list item
-            $('.portfolio li.hide').removeClass('hide');
-            // Reset positioning
-            $('.modal-image').removeAttr('style');
-        }, 350);
-    },400);
+            // Move it back to the list and disappear
+            $('.modal-image').removeClass('animate-in').css({
+                transform : 'translateX(0) translateY(0)',
+                width : $('.portfolio li').innerWidth()
+            });
+            
+            // Wait for transition to finish...
+            setTimeout(function() {
+                // Re-enable scroll
+                $('body').removeClass('modal-open');
+                
+                // Show list item
+                $('.portfolio li.hide').removeClass('hide');
+                
+                // Reset positioning
+                $('.modal-image').removeAttr('style');
+            }, 400);
+
+        },400);
+    }
 });
